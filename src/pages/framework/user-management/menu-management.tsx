7b8bfaeefe71ec690a7eefae811e4602f569dbfb
gunjoopark <122519183+gunjoopark@users.noreply.github.com>
2023-11-24 18:20:26 +0900

    menumanagement

--- src/pages/framework/user-management/menu-management.tsx
+++ src/pages/framework/user-management/menu-management.tsx
@@ -1,3 +1,71 @@
+import {DropDownList} from "@progress/kendo-react-dropdowns";
+import {PAGES, SPORTS} from "@/constants";
+import {Input} from "@progress/kendo-react-inputs";
+import {Button} from "@progress/kendo-react-buttons";
+import {arrowRightIcon, searchIcon} from "@progress/kendo-svg-icons";
+import { MenuManagementTable } from "@/components/MenuManagementTable";
+
+
 export function Page() {
-  return <div>Works!</div>;
+    return (
+        <>
+            <>
+                <div className="flex items-center gap-2 py-4">
+                    <img src={"/images/dot_subtitle.gif"} alt="" style={{}}/>
+                    <span>Condition</span>
+                </div>
+                <div className="searchbox flex justify-between max-h-[40px] min-h-[40px]">
+                    <div className="flex items-center gap-4">
+                        <div className="minw input-group" style={{minWidth: '120px', marginLeft: '2px'}}>
+                            <DropDownList
+                                className="w-32 h-7 text-xs mr-2 ml-5"
+                                size={"small"}
+                                data={SPORTS}
+                                defaultValue="Option 1"
+                                filterable={false}
+                            />
+
+                            <Input className="w-40 h-7"/>
+                        </div>
+
+                        <div className="minw input-group" style={{minWidth: '120px', marginLeft: '2px'}}>
+                            <span className="text-xs mr-2">Menu URL</span>
+                            <Input className="w-40 h-7"/>
+                        </div>
+
+                        <div className="minw input-group" style={{minWidth: '120px', marginLeft: '2px'}}>
+                            <span className="text-xs mr-2">Top menu ID</span>
+                            <Input className="w-40 h-7"/>
+                        </div>
+                        <button data-role="button" role="button" className="search_btn no-text" aria-disabled="false">
+                            <img src="/images/search.gif" alt=""/>
+                        </button>
+                    </div>
+
+                    <div className="flex items-center gap-8">
+                        <div className="flex items-center gap-2">
+                            <DropDownList className="w-16 h-7" size={"small"} data={PAGES} defaultValue="20"
+                                          filterable={false}/>
+                            <span className="text-xs">Items</span>
+                        </div>
+
+                        <Button imageUrl="/images/refresh.png" className="find">Find</Button>
+                    </div>
+                </div>
+            </>
+            <>
+                <div className="flex items-center gap-2 pb-4">
+                    <img src={"/images/dot_subtitle.gif"} alt="" style={{}}/>
+                    <span>List</span>
+                </div>
+                <MenuManagementTable/>
+            </>
+
+            <div className="flex justify-end">
+                <Button svgIcon={arrowRightIcon} className="flex items-center justify-end  mt-2">
+                    ADD
+                </Button>
+            </div>
+        </>
+    );
 }
