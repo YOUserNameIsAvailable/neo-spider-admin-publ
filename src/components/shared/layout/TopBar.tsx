de02504f3b36ac5c6f2efbeb4ab61fe0c58652d8
jonghwaWon <jh.won@teamflyers.kr>
2023-11-24 11:26:05 +0900

    TopBar edit

--- src/components/shared/layout/TopBar.tsx
+++ src/components/shared/layout/TopBar.tsx
@@ -4,7 +4,7 @@ import { AppBar, AppBarSection, AppBarSpacer } from "@progress/kendo-react-layou
 export function TopBar() {
   return (
     <>
-      <AppBar>
+      <AppBar style={{background:'url(./images/bg_topcenter.gif)',height:'53px',}}>
         <AppBarSpacer />
 
         <AppBarSection className="actions"></AppBarSection>
@@ -12,15 +12,18 @@ export function TopBar() {
         <AppBarSection>
           <ul className="text-sm list-none flex m-0 p-0 items-center [&>li]:mx-2.5 my-0">
             <li>
-              <span>Admin</span>
+              <span className="text-white p-[10px] text-[16px]">Admin</span>
             </li>
             <li>
-              <span>Login OK.</span>
+            <span className="text-white text-[16px]">님께서 접속하셨습니다.</span>
             </li>
             <li>
-              <Button size={"small"} themeColor={"primary"}>
-                Logout
-              </Button>
+              <Button style={{background:'url(./images/btn_logout_off.gif)',backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        width:"70px"
+                        ,height:"30px"
+                        }} />
             </li>
           </ul>
         </AppBarSection>
