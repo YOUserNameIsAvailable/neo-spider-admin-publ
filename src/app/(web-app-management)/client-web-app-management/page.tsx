d2159e984367fbd5dd44789979ad0b2a7fd04e78
chanlee <pck886@naver.com>
2023-11-27 19:23:47 +0900

    client-web-app-management

--- src/app/(web-app-management)/client-web-app-management/page.tsx
+++ src/app/(web-app-management)/client-web-app-management/page.tsx
@@ -6,196 +6,22 @@ import { Button } from "@progress/kendo-react-buttons";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
 import { searchIcon, arrowRightIcon } from "@progress/kendo-svg-icons";
 import { useState } from "react";
-import { ClientWebTable } from "@/components/ClientWebTable";
-import { PAGES, SPORTS } from "@/constants";
+import { ClientWebAppMain } from "./client-web-app-management-main";
+import { ClientWebAppDetail } from "./client-web-app-management-detail";
 
 export default function Page() {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isDetail, setIsDetail] = useState(false);
 
-  const toggleExpansion = () => {
-    setIsExpanded(!isExpanded);
+  console.log("client-web-app-management isDetail: ", isDetail);
+
+  const onRowClick = (event: any) => {
+    console.log("client-web-app-management onRowClick: ", event.dataItem);
+    setIsDetail((prev) => !prev);
   };
-  return (
-    <>
-      {/* filters */}
-      <div>
-        <div className="flex items-center gap-2 py-4">
-          <img src={"/images/dot_subtitle.gif"} alt="" style={{}} />
-          <span>Condition</span>
-          <button className="bg-neutral-50 p-2" onClick={() => toggleExpansion()}>
-            Enlargement/Reduction
-          </button>
-        </div>
-        <div className="flex justify-between gap-4 bg-neutral-50 p-4">
-          <div className="flex items-center gap-4">
-            <div className="flex items-center gap-2">
-              <span className="text-sm">Menu url</span>
-              <Input className="h-7 w-40" />
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="text-sm">Menu name</span>
-              <Input className="h-7 w-40" />
-            </div>
-          </div>
 
-          <div className="flex items-center gap-8">
-            <div className="flex items-center gap-2">
-              <DropDownList className="h-7 w-16" size={"small"} data={PAGES} defaultValue="20" filterable={false} />
-              <span className="text-sm">Items</span>
-            </div>
-
-            <Button svgIcon={searchIcon}>Find</Button>
-          </div>
-        </div>
-        <div className="border-grey-500 flex justify-between gap-4 border-t-2 bg-neutral-50  p-4">
-          <div className="flex items-center gap-4">
-            <div className="flex items-center gap-2">
-              <span className="text-sm">Custom Action class</span>
-              <Input className="h-7 w-40" />
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="text-sm">View number</span>
-              <Input className="h-7 w-40" />
-            </div>
-          </div>
-        </div>
-        <div className="border-grey-500 flex justify-between gap-4 border-t-2 bg-neutral-50  p-4">
-          <div className="flex items-center gap-4">
-            <div className="flex items-center gap-2">
-              <span className="text-sm">Site type</span>
-              <DropDownList
-                className="h-7 w-40"
-                size={"small"}
-                data={SPORTS}
-                defaultValue="Option 1"
-                filterable={false}
-              />
-            </div>
-
-            <div className="flex items-center gap-2">
-              <span className="text-sm">Check status of other banks</span>
-              <DropDownList
-                className="h-7 w-40"
-                size={"small"}
-                data={SPORTS}
-                defaultValue="Option 1"
-                filterable={false}
-              />
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="text-sm">Service state</span>
-              <DropDownList
-                className="h-7 w-40"
-                size={"small"}
-                data={SPORTS}
-                defaultValue="Option 1"
-                filterable={false}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      {isExpanded ? (
-        <>
-          {" "}
-          <div className="border-grey-500 flex justify-between gap-4 border-t-2 bg-neutral-50  p-4">
-            <div className="flex items-center gap-4">
-              <div className="flex items-center gap-2">
-                <span className="text-sm">e채널 로그 분류코드</span>
-                <DropDownList
-                  className="h-7 w-40"
-                  size={"small"}
-                  data={SPORTS}
-                  defaultValue="Option 1"
-                  filterable={false}
-                />
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="text-sm">Required</span>
-                <DropDownList
-                  className="h-7 w-40"
-                  size={"small"}
-                  data={SPORTS}
-                  defaultValue="Option 1"
-                  filterable={false}
-                />
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="text-sm">Whether electronic signature is required</span>
-                <DropDownList
-                  className="h-7 w-40"
-                  size={"small"}
-                  data={SPORTS}
-                  defaultValue="Option 1"
-                  filterable={false}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="border-grey-500 flex justify-between gap-4 border-t-2 bg-neutral-50  p-4">
-            <div className="flex items-center gap-4">
-              <div className="flex items-center gap-2">
-                <span className="text-sm">입력유형</span>
-                <DropDownList
-                  className="h-7 w-40"
-                  size={"small"}
-                  data={SPORTS}
-                  defaultValue="Option 1"
-                  filterable={false}
-                />
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="text-sm">IN/OUTuse message</span>
-                <DropDownList
-                  className="h-7 w-40"
-                  size={"small"}
-                  data={SPORTS}
-                  defaultValue="Option 1"
-                  filterable={false}
-                />
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="text-sm">Biz class</span>
-                <DropDownList
-                  className="h-7 w-40"
-                  size={"small"}
-                  data={SPORTS}
-                  defaultValue="Option 1"
-                  filterable={false}
-                />
-              </div>
-            </div>
-          </div>
-        </>
-      ) : null}
-      {/* table */}
-
-      <div className="flex w-[100%]">
-        <div className="flex w-[50%] items-center gap-2 py-4">
-          <img src={"/images/dot_subtitle.gif"} alt="" style={{}} />
-          <span>List</span>
-        </div>
-        <div className="flex w-[90%] gap-4 ">
-          <Button svgIcon={arrowRightIcon} className="w-30 mt-2 flex h-7  items-center justify-start ">
-            stop selection
-          </Button>
-          <Button svgIcon={arrowRightIcon} className="w-46 mt-2 flex h-7 items-center justify-start">
-            Selection guide message
-          </Button>
-          <Button svgIcon={arrowRightIcon} className="mt-2 flex h-7 w-32 items-center justify-start">
-            Group manage
-          </Button>
-          <Button svgIcon={arrowRightIcon} className="mt-2 flex h-7 w-32 items-center justify-start">
-            Del select
-          </Button>
-        </div>
-      </div>
-      <ClientWebTable />
-      <div className="flex justify-end">
-        <Button svgIcon={arrowRightIcon} className="mt-2 flex items-center  justify-end">
-          ADD
-        </Button>
-      </div>
-    </>
+  return !isDetail ? (
+    <ClientWebAppMain onRowClick={onRowClick} />
+  ) : (
+    <ClientWebAppDetail setIsDetail={setIsDetail} />
   );
 }
