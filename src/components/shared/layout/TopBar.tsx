88c6365ee73cd77b0eb9ca70ee68cf1a7098eabc
chanlee <pck886@naver.com>
2023-11-27 19:15:52 +0900

    merge

--- src/components/shared/layout/TopBar.tsx
+++ src/components/shared/layout/TopBar.tsx
@@ -3,31 +3,33 @@ import { AppBar, AppBarSection, AppBarSpacer } from "@progress/kendo-react-layou
 
 export function TopBar() {
   return (
-    <>
-      <AppBar style={{background:'url(./images/bg_topcenter.gif)',height:'53px',}}>
-        <AppBarSpacer />
+    <AppBar style={{ background: "url(./images/bg_topcenter.gif)", height: "53px" }}>
+      <AppBarSpacer />
 
-        <AppBarSection className="actions"></AppBarSection>
+      <AppBarSection className="actions"></AppBarSection>
 
-        <AppBarSection>
-          <ul className="text-sm list-none flex m-0 p-0 items-center [&>li]:mx-2.5 my-0">
-            <li>
-              <span className="text-white p-[10px] text-[16px]">Admin</span>
-            </li>
-            <li>
-            <span className="text-white text-[16px]">님께서 접속하셨습니다.</span>
-            </li>
-            <li>
-              <Button style={{background:'url(./images/btn_logout_off.gif)',backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
-                        width:"70px"
-                        ,height:"30px"
-                        }} />
-            </li>
-          </ul>
-        </AppBarSection>
-      </AppBar>
-    </>
+      <AppBarSection>
+        <ul className="m-0 my-0 flex list-none items-center p-0 text-sm [&>li]:mx-2.5">
+          <li>
+            <span className="p-[10px] text-[16px] text-white">Admin</span>
+          </li>
+          <li>
+            <span className="text-[16px] text-white">님께서 접속하셨습니다.</span>
+          </li>
+          <li>
+            <Button
+              style={{
+                background: "url(./images/btn_logout_off.gif)",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                width: "70px",
+                height: "30px",
+              }}
+            />
+          </li>
+        </ul>
+      </AppBarSection>
+    </AppBar>
   );
 }
