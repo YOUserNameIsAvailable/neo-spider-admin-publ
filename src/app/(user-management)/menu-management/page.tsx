40375cf49b664484605b8c703a47df87d8efe37d
gunjoopark <122519183+gunjoopark@users.noreply.github.com>
2023-11-29 18:44:38 +0900

    Update page.tsx

--- src/app/(user-management)/menu-management/page.tsx
+++ src/app/(user-management)/menu-management/page.tsx
@@ -4,15 +4,34 @@ import { DropDownList } from "@progress/kendo-react-dropdowns";
 import { PAGES, SPORTS } from "@/constants";
 import { Input } from "@progress/kendo-react-inputs";
 import { Button } from "@progress/kendo-react-buttons";
-import { arrowRightIcon } from "@progress/kendo-svg-icons";
 import { MenuManagementTable } from "@/components/MenuManagementTable";
 import React from "react";
+import { Window, WindowMoveEvent } from "@progress/kendo-react-dialogs";
 
 export default function Page() {
+  const [visible, setVisible] = React.useState(false);
+  const [position, setPosition] = React.useState({
+    left: 400,
+    top: 182,
+    width: 1000,
+    height: 700,
+  });
+  const handleMove = (event: WindowMoveEvent) => {
+    setPosition({ ...position, left: event.left, top: event.top });
+  };
+  const handleResize = (event: WindowMoveEvent) => {
+    setPosition({
+      left: event.left,
+      top: event.top,
+      width: event.width,
+      height: event.height,
+    });
+  };
+
   return (
     <>
       <>
-        <div className="flex items-center gap-2 py-4">
+        <div className="flex items-center gap-2 py-4" onClick={() => setVisible(true)}>
           <img src={"/images/dot_subtitle.gif"} alt="" />
           <span className="font-bold text-[#656565]">Condition</span>
         </div>
@@ -70,6 +89,143 @@ export default function Page() {
           ADD
         </Button>
       </div>
+      {visible && (
+        <>
+          <div className="k-overlay" />
+          <Window
+            minimizeButton={() => null}
+            maximizeButton={() => null}
+            restoreButton={() => null}
+            doubleClickStageChange={false}
+            title={"컴포넌트 정보"}
+            // left={position.left}
+            // top={position.top}
+            width={position.width}
+            height={position.height}
+            onMove={handleMove}
+            onResize={handleResize}
+            onClose={() => {
+              setVisible(false);
+            }}>
+            <div className="flex flex-col gap-[15px]">
+              <div className="flex flex-col gap-[12px]">
+                <div className="flex items-center gap-1 pb-[4px]">
+                  <img src="./images/dot_subtitle.gif" className="h-[12px] w-[12px]" />
+                  <div className="text-[14px] font-bold text-[#656565]">컴포넌트 정보</div>
+                </div>
+              </div>
+              <div className="flex h-full w-full flex-row">
+                <div className="flex w-[60%] flex-col border-[1px] border-[#dfe1e1] bg-red-100">
+                  <div className="flex h-[29px] flex-row border-b-[1px] border-[#dfe1e1]">
+                    <div className="flex w-[70%] flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        업무분류
+                      </label>
+                      <input className="my-[2px] ml-[2px] w-[150px] rounded-[2px] border-[1px] border-[#999999] py-[2px]" />
+                    </div>
+                    <div className="flex w-[30%] flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        유형
+                      </label>
+                      <DropDownList
+                        style={{ width: "40%", marginRight: "2px", fontSize: "12px", marginLeft: "2px" }}
+                        size={"small"}
+                        data={["선택 안 함", "안전", "주의", "경계"]}
+                        defaultValue={"선택 안 함"}
+                        className="my-[2px]"
+                      />
+                      <span className="required">*</span>
+                    </div>
+                  </div>
+                  {/*  */}
+                  <div className="flex h-[29px] flex-row border-b-[1px] border-[#dfe1e1]">
+                    <div className="flex w-full flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        컴포넌트ID
+                      </label>
+                      <input className="my-[2px] ml-[2px] w-[170px] rounded-[2px] border-[1px] border-[#999999] py-[2px]" />
+                      <span className="required">*</span>
+                    </div>
+                  </div>
+                  {/*  */}
+                  <div className="flex h-[29px] flex-row border-b-[1px] border-[#dfe1e1]">
+                    <div className="flex w-full flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        컴포넌트명
+                      </label>
+                      <input className="my-[2px] ml-[2px] w-[170px] rounded-[2px] border-[1px] border-[#999999] py-[2px]" />
+                      <span className="required">*</span>
+                    </div>
+                  </div>
+                  {/*  */}
+                  <div className="flex h-[29px] flex-row border-b-[1px] border-[#dfe1e1]">
+                    <div className="flex w-full flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        경로
+                      </label>
+                      <input className="my-[2px] ml-[2px] w-[250px] rounded-[2px] border-[1px] border-[#999999] py-[2px]" />
+                      <span className="required">*</span>
+                    </div>
+                    <div className="ml-4 flex flex w-full flex-row items-center">http://</div>
+                  </div>
+                  {/*  */}
+                  <div className="flex h-[29px] flex-row border-b-[1px] border-[#dfe1e1]">
+                    <div className="flex w-full flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        논리경로
+                      </label>
+                      <input className="my-[2px] ml-[2px] w-[250px] rounded-[2px] border-[1px] border-[#999999] py-[2px]" />
+                      <span className="required">*</span>
+                    </div>
+                    <div className="ml-4 flex flex w-full flex-row items-center gap-2">
+                      <Button
+                        imageUrl="/images/dot-right-arrow.png"
+                        className="basic-btn flex items-center justify-start">
+                        폴더선택
+                      </Button>
+                      <Button
+                        imageUrl="/images/dot-right-arrow.png"
+                        className="basic-btn flex items-center justify-start">
+                        폴더(로컬)
+                      </Button>
+                    </div>
+                  </div>
+                  {/*  */}
+                  <div className="flex h-[29px] flex-row border-b-[1px] border-[#dfe1e1]">
+                    <div className="flex w-full flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        수정자
+                      </label>
+                      <input className="my-[2px] ml-[2px] w-[200px] rounded-[2px] border-[1px] border-[#999999] py-[2px]" />
+                    </div>
+                  </div>
+                  {/*  */}
+                  <div className="flex h-[45px] flex-row border-b-[1px] border-[#dfe1e1]">
+                    <div className="flex w-full flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        설명
+                      </label>
+                      <textarea className="my-[2px] ml-[2px] w-[430px] resize-none rounded-[2px] border-[1px] border-[#999999] py-[2px]" />
+                      <span className="required">*</span>
+                    </div>
+                  </div>
+                  {/*  */}
+                  <div className="flex h-[44px] flex-row">
+                    <div className="flex w-full flex-row">
+                      <label className="flex h-full w-[70px]  items-center bg-[#d1daec] p-[4px] text-[12px] text-black">
+                        상세설명
+                      </label>
+                      <textarea className="my-[2px] ml-[2px] w-[430px] resize-none rounded-[2px] border-[1px] border-[#999999] py-[2px]" />
+                      <span className="required">*</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex h-[100px] w-[40%] bg-blue-100"></div>
+              </div>
+            </div>
+          </Window>
+        </>
+      )}
     </>
   );
 }
